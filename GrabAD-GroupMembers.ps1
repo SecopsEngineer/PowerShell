07a1fcdc4ed6a285bca9cc0e1b210c3fd29d1876
John Skinner <91431028+SecopsEngineer@users.noreply.github.com>
2021-09-29 17:28:04 -0500

    AD Group Member Extract
    Simple script to prompt for an AD Group and output all the group members to a CSV file

---
+++ GrabAD-GroupMembers.ps1
@@ -0,0 +1,39 @@
+﻿Function Get-ADGroupMemberJohn {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0
+        )]
+        [string[]]
+        $Identity
+    )
+    process {
+        foreach ($GroupIdentity in $Identity) {
+            $Group = $null
+            $Group = Get-ADGroup -Identity $GroupIdentity -Properties Member -server bbc
+            if (-not $Group) {
+                continue
+            }
+            Foreach ($Member in $Group.Member) {
+                Get-ADUser $Member -server bbc |Select-object -ExpandProperty name
+            }
+        }
+    }
+}
+Write-Host "***************************************************************" -ForegroundColor cyan
+Write-Host "Welcome to your Friendly Nieghborhood AD Group Member Extractor" -ForegroundColor cyan
+Write-Host "***************************************************************" -ForegroundColor cyan
+
+$x = "go"
+do {
+    $group = read-host "What group do you want?"
+    Get-ADGroupMemberJohn $group | Out-File C:\temp\MFAUsers.csv
+    Write-Host "Your Group Member list is stored in c:\Temp\MFAUsers.csv" -ForgroundColor green
+    $answer = Read-host "another one?"
+    if ($answer -eq "no" -or $answer -eq "n"){$x = "stop"}
+    cls
+    }
+    while ($x -ne "stop")
