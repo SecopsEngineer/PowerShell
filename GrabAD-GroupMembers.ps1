65470d1cffeefcb67b1c245ba6fcee089d04367e
John Skinner <91431028+SecopsEngineer@users.noreply.github.com>
2021-10-08 18:51:05 -0500

    Get-AD Group Members
    A simple script to extract group members from an AD Group... please note the comments
    
    #For this screipt to work you will need to change lines 18 and 23 
    #the "-server domainname" should be the name of the domain tha tyou want to search

---
+++ GrabAD-GroupMembers.ps1
@@ -0,0 +1,42 @@
+﻿#For this screipt to work you will need to change lines 18 and 23 
+#the "-server domainname" should be the name of the domain tha tyou want to search
+
+Function Get-ADGroupMemberJohn {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0
+        )]
+        [string[]]
+        $Identity
+    )
+    process {
+        foreach ($GroupIdentity in $Identity) {
+            $Group = $null
+            $Group = Get-ADGroup -Identity $GroupIdentity -Properties Member -server domainname
+            if (-not $Group) {
+                continue
+            }
+            Foreach ($Member in $Group.Member) {
+                Get-ADUser $Member -server domainname |Select-object -ExpandProperty name
+            }
+        }
+    }
+}
+Write-Host "***************************************************************" -ForegroundColor cyan
+Write-Host "Welcome to your Friendly Nieghborhood AD Group Member Extractor" -ForegroundColor cyan
+Write-Host "***************************************************************" -ForegroundColor cyan
+
+$x = "go"
+do {
+    $group = read-host "What group do you want?"
+    Get-ADGroupMemberJohn $group | Out-File C:\temp\MFAUsers.csv
+    Write-Host "Your Group Member list is stored in c:\Temp\MFAUsers.csv" -ForgroundColor green
+    $answer = Read-host "another one?"
+    if ($answer -eq "no" -or $answer -eq "n"){$x = "stop"}
+    cls
+    }
+    while ($x -ne "stop")
